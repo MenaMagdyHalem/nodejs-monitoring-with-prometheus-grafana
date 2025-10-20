4ebd660a97c79d845e6c27e3dd9f7bedf23296a1
Mena Magdy Halem <44339796+MenaMagdyHalem@users.noreply.github.com>
2025-10-20 16:44:36 +0300

    Update send.sh

--- nodejs-app/send.sh
+++ nodejs-app/send.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+send_requests() {
+  while true; do
+    curl -sS http://<node-ip>:<nodeport> > /dev/null
+    echo "Request sent"
+    sleep 0.0667
+  done
+}
+
+for ((i=1; i<=15; i++)); do
+  send_requests &
+  pids[$i]=$!
+done
+
+trap 'echo "Exiting..."; kill ${pids[*]}; exit' INT
+wait
 
